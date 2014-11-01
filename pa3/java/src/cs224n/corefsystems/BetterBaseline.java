d84cbbdbf989340760e681b650c764cc3c9f93f6
tongzhang54 <panda90926@gmail.com>
2014-10-31 20:44:47 -0700

    BetterBaseline Updated

--- pa3/java/src/cs224n/corefsystems/BetterBaseline.java
+++ pa3/java/src/cs224n/corefsystems/BetterBaseline.java
@@ -10,11 +10,21 @@ import cs224n.util.Pair;
 
 public class BetterBaseline implements CoreferenceSystem {
 
-    private Set<Pair<String, String>> headWordPairSet;
+    private Set<Set<String>> headWordGroups = new HashSet<Set<String>>();
+    private Set<String> possibleHeadWords = new HashSet<String>();
+
+    private Set<String> findHeadWordGroup(String headWord) {
+        for (Set<String> group : headWordGroups) {
+            if (group.contains(headWord)) {
+                return group;
+            }
+        }
+        return null;
+    }
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		headWordPairSet = new HashSet<Pair<String, String>>();
+
         for(Pair<Document, List<Entity>> pair : trainingData){
             List<Entity> clusters = pair.getSecond();
 
@@ -22,10 +32,51 @@ public class BetterBaseline implements CoreferenceSystem {
                 for(Pair<Mention, Mention> mentionPair : cluster.orderedMentionPairs()) {
                     Mention m1 = mentionPair.getFirst();
                     Mention m2 = mentionPair.getSecond();
-                    String headWord1 = m1.headWord();
-                    String headWord2 = m2.headWord();
-                    Pair<String, String> headWordPair = new Pair<String, String>(headWord1, headWord2);
-                    headWordPairSet.add(headWordPair);
+                    String hw1 = m1.headWord();
+                    String hw2 = m2.headWord();
+
+                    if (!possibleHeadWords.contains(hw1) && !possibleHeadWords.contains(hw2)) {
+                        Set<String> newSet = new HashSet<String>();
+                        newSet.add(hw1);
+                        newSet.add(hw2);
+                        headWordGroups.add(newSet);
+                        possibleHeadWords.addAll(newSet);
+                    } else if (!possibleHeadWords.contains(hw1)) {
+                        Set<String> group = findHeadWordGroup(hw2);
+                        if (group != null) {
+                            headWordGroups.remove(group);
+                            group.add(hw1);
+                            headWordGroups.add(group);
+                            possibleHeadWords.add(hw1);
+                        }
+                    } else if (!possibleHeadWords.contains(hw2)) {
+                        Set<String> group = findHeadWordGroup(hw1);
+                        if (group != null) {
+                            headWordGroups.remove(group);
+                            group.add(hw2);
+                            headWordGroups.add(group);
+                            possibleHeadWords.add(hw2);
+                        }
+                    } else {
+                        Set<String> group1 = findHeadWordGroup(hw1);
+                        Set<String> group2 = findHeadWordGroup(hw2);
+
+                        if (group1 != null && group2 != null) {
+                            headWordGroups.remove(group1);
+                            headWordGroups.remove(group2);
+                            group1.addAll(group2);
+                            headWordGroups.add(group1);
+                        } else if (group1 != null) {
+                            headWordGroups.remove(group1);
+                            group1.add(hw2);
+                            headWordGroups.add(group1);
+                        } else if (group2 != null) {
+                            headWordGroups.remove(group2);
+                            group2.add(hw1);
+                            headWordGroups.add(group2);
+                        }
+                    }
+
                 }
 
             }
@@ -37,19 +88,34 @@ public class BetterBaseline implements CoreferenceSystem {
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
         List<ClusteredMention> clusteredMentions = new ArrayList<ClusteredMention>();
-        Map<String, Entity> headWordClusterMap = new HashMap<String, Entity>();
-        for(Mention m : doc.getMentions()){
+        Map<Set<String>, Entity> headWordsClusterMap = new HashMap<Set<String>, Entity>();
+
+        for (Mention m : doc.getMentions()) {
             String headWord = m.headWord();
-            if (headWordClusterMap.containsKey(headWord)) {
-                Entity cluster = headWordClusterMap.get(headWord);
-                clusteredMentions.add(m.markCoreferent(cluster));
+            if (possibleHeadWords.contains(headWord)) {
+                Set<String> group = findHeadWordGroup(headWord);
+                if (group != null) {
+                    if (headWordsClusterMap.containsKey(group)) {
+                        Entity cluster = headWordsClusterMap.get(group);
+                        clusteredMentions.add(m.markCoreferent(cluster));
+                    } else {
+                        Entity cluster = new Entity(new ArrayList<Mention>(), m);
+                        clusteredMentions.add(m.markCoreferent(cluster));
+                        headWordsClusterMap.put(group, cluster);
+                    }
+                } else {
+                    System.out.println("Group == null!");
+                }
+
             } else {
+                Set<String> group = new HashSet<String>();
+                group.add(headWord);
                 Entity cluster = new Entity(new ArrayList<Mention>(), m);
                 clusteredMentions.add(m.markCoreferent(cluster));
-                headWordClusterMap.put(headWord, cluster);
+                headWordsClusterMap.put(group, cluster);
             }
-
         }
+
         return clusteredMentions;
     }
 
