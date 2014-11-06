6b3c401c3f8e5605bf2a69ffbebff65c98ba5cbb
tongzhang54 <panda90926@gmail.com>
2014-11-06 10:24:54 -0800

    Refined pronoun rules dev score MUC .767 B3 .717

--- pa3/java/src/cs224n/corefsystems/RuleBased.java
+++ pa3/java/src/cs224n/corefsystems/RuleBased.java
@@ -2,10 +2,7 @@ package cs224n.corefsystems;
 
 import java.util.*;
 
-import cs224n.coref.ClusteredMention;
-import cs224n.coref.Document;
-import cs224n.coref.Entity;
-import cs224n.coref.Mention;
+import cs224n.coref.*;
 import cs224n.util.Pair;
 
 public class RuleBased implements CoreferenceSystem {
@@ -22,19 +19,28 @@ public class RuleBased implements CoreferenceSystem {
         List<ClusteredMention> clusteredMentions = new ArrayList<ClusteredMention>();
 
         Set<Set<Mention>> mentionGroups = new HashSet<Set<Mention>>();
+        Set<Set<Mention>> pronounMentionGroups = new HashSet<Set<Mention>>();
 
         // Initialize each mention to its own group
         for (Mention m : doc.getMentions()) {
             Set<Mention> singleMentionGroup = new HashSet<Mention>();
             singleMentionGroup.add(m);
-            mentionGroups.add(singleMentionGroup);
+            if (Pronoun.valueOrNull(m.headWord()) != null) {
+                pronounMentionGroups.add(singleMentionGroup);
+            } else {
+                String nerTag = m.headToken().nerTag();
+
+                mentionGroups.add(singleMentionGroup);
+            }
+//            System.out.println("NER Tag: " + m.headWord() + " " + m.headToken().nerTag());
+//            System.out.println("Mention Parse: " + m.parse);
         }
 
-        // Exact String Match
-        for (Set<Mention> group1 : mentionGroups) {
-            for (Set<Mention> group2 : mentionGroups) {
+        // Pronoun Match
+        for (Set<Mention> group1 : pronounMentionGroups) {
+            for (Set<Mention> group2 : pronounMentionGroups) {
                 if (!group1.equals(group2)) {
-                    boolean shouldMerge = exactStringMatch(group1, group2);
+                    boolean shouldMerge = pronounMatch(group1, group2);
                     if (shouldMerge) {
                         group1.addAll(group2);
                         group2.removeAll(group2);
@@ -43,11 +49,11 @@ public class RuleBased implements CoreferenceSystem {
             }
         }
 
-        // Head Word Match
+        // Exact String Match
         for (Set<Mention> group1 : mentionGroups) {
             for (Set<Mention> group2 : mentionGroups) {
                 if (!group1.equals(group2)) {
-                    boolean shouldMerge = headWordMatch(group1, group2);
+                    boolean shouldMerge = exactStringMatch(group1, group2);
                     if (shouldMerge) {
                         group1.addAll(group2);
                         group2.removeAll(group2);
@@ -69,6 +75,30 @@ public class RuleBased implements CoreferenceSystem {
             }
         }
 
+        // Head Token Noun Match
+        for (Set<Mention> group1 : mentionGroups) {
+            for (Set<Mention> group2 : mentionGroups) {
+                if (!group1.equals(group2)) {
+                    boolean shouldMerge = mentionParseMatch(group1, group2);
+                    if (shouldMerge) {
+                        group1.addAll(group2);
+                        group2.removeAll(group2);
+                    }
+                }
+            }
+        }
+
+        for (Set<Mention> group : pronounMentionGroups) {
+            List<Mention> mentionList = new ArrayList<Mention>();
+            mentionList.addAll(group);
+            Entity cluster = new Entity(mentionList);
+            for (Mention m : group) {
+                ClusteredMention clusteredMention = m.markCoreferent(cluster);
+                clusteredMentions.add(clusteredMention);
+            }
+        }
+
+
         for (Set<Mention> group : mentionGroups) {
             List<Mention> mentionList = new ArrayList<Mention>();
             mentionList.addAll(group);
@@ -82,6 +112,36 @@ public class RuleBased implements CoreferenceSystem {
 
         return clusteredMentions;
 	}
+
+    private boolean pronounMatch(Set<Mention> group1, Set<Mention> group2) {
+        for (Mention m1 : group1) {
+            Pronoun pronoun1 = Pronoun.valueOrNull(m1.headWord());
+            if (pronoun1 == null) {
+                break;
+            }
+            for (Mention m2 : group2) {
+                Pronoun pronoun2 = Pronoun.valueOrNull(m2.headWord());
+                if (pronoun2 == null) {
+                    break;
+                }
+                if (pronoun1.gender == pronoun2.gender
+                        && pronoun1.speaker == pronoun2.speaker
+                        && pronoun1.plural == pronoun2.plural) {
+                    if (pronoun1.speaker != Pronoun.Speaker.THIRD_PERSON) {
+                        return true;
+                    } else {
+                        int index1 = m1.doc.indexOfSentence(m1.sentence);
+                        int index2 = m2.doc.indexOfSentence(m2.sentence);
+                        if (Math.abs(index1 - index2) <= 1) {
+                            return true;
+                        }
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
 
 
     private boolean exactStringMatch(Set<Mention> group1, Set<Mention> group2) {
@@ -110,6 +170,51 @@ public class RuleBased implements CoreferenceSystem {
         for (Mention m1 : group1) {
             for (Mention m2 : group2) {
                 if (m1.headToken().lemma().equalsIgnoreCase(m2.headToken().lemma())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean headTokenPluralNounMatch(Set<Mention> group1, Set<Mention> group2) {
+        for (Mention m1 : group1) {
+            for (Mention m2 : group2) {
+                if (m1.headToken().isPluralNoun() && m2.headToken().isPluralNoun()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean headTokenSpeakerMatch(Set<Mention> group1, Set<Mention> group2) {
+        for (Mention m1 : group1) {
+            for (Mention m2 : group2) {
+                if (m1.headToken().speaker().equals(m2.headToken().speaker())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean headTokenNERMatch(Set<Mention> group1, Set<Mention> group2) {
+        for (Mention m1 : group1) {
+            for (Mention m2 : group2) {
+                if (m1.headToken().nerTag().equals(m2.headToken().nerTag())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean mentionParseMatch(Set<Mention> group1, Set<Mention> group2) {
+        for (Mention m1 : group1) {
+            for (Mention m2 : group2) {
+                if (m1.parse.equals(m2.parse)) {
                     return true;
                 }
             }
